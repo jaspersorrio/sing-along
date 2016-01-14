46601e96c6f3bf9284c9e5b050d5a72a00c74546
jaspersorriso <s10149149@connect.np.edu.sg>
2016-01-14 18:06:45 +0800

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name singalongApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the singalongApp
+ */
+angular.module('singalongApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name singalongApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the singalongApp
+ */
+angular.module('singalongApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
